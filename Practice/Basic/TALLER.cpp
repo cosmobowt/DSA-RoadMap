7bba6a9bbca4dd6a025c316d56f1121fb086d196
AdityA <aditya.ace93@gmail.com>
2022-08-11 11:49:35 +0530

    Create TALLER.cpp

---
+++ Practice/Basic/TALLER.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int times, x, y;
+	
+	cin >> times; 
+	while(times--){
+	    cin >> x >> y;A
+	    if (x > y){
+	        cout << "A" << endl;
+	    }
+	    
+	    else{
+	        cout << "B" << endl;
+	    }
+	}
+	return 0;
+}
