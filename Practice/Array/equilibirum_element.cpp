6e072277e2197920ccdc0383c3efbd990ee67ab8
AdityA <aditya.ace93@gmail.com>
2022-08-30 00:07:04 +0530

    Update equilibirum_element.cpp

--- Practice/Array/equilibirum_element.cpp
+++ Practice/Array/equilibirum_element.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby
+GDB online is an online compiler and debugger tool for C, C++, Python, Java
 
 *******************************************************************************/
 #include <iostream>
