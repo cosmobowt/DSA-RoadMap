da5ea5674630eb879583ae7f7d4d1f4a81cca1df
AdityA <aditya.ace93@gmail.com>
2022-12-27 00:04:15 +0530

    Update linear_traversal_search.cpp

--- RoadMap/Array/linear_traversal_search.cpp
+++ RoadMap/Array/linear_traversal_search.cpp
@@ -4,7 +4,6 @@
                               Online C++ Compiler.
                Code, Compile, Run and Debug C++ program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
 ***********************************************************************/
 
 #include <iostream>
