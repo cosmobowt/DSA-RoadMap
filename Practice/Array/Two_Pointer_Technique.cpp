d453f46fccfac1d8ee9433a9b7a1a2fd9cdd2c62
AdityA <aditya.ace93@gmail.com>
2023-03-05 22:20:50 +0530

    Update Two_Pointer_Technique.cpp

--- Practice/Array/Two_Pointer_Technique.cpp
+++ Practice/Array/Two_Pointer_Technique.cpp
@@ -5,7 +5,7 @@ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS
 Code, Compile, Run and Debug online from anywhere in world.
 ******************************************************************************/
 #include <iostream>
-
+ 
 using namespace std;
 
 int result(int arr[], int n, int val){
