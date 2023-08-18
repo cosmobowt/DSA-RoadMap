a7674311725d260cbb9c4b66c6570e0cbd97d3ad
AdityA <aditya.ace93@gmail.com>
2023-08-18 19:12:14 +0530

    Update equilibirum_element.cpp

--- Practice/Array/equilibirum_element.cpp
+++ Practice/Array/equilibirum_element.cpp
@@ -8,7 +8,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <iostream>      
   
 using namespace std; 
-  
+   
 int equilibrium(int arr[], int n){
      
     int leftSum = 0;
