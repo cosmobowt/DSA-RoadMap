e41b2a68ddb8f5e7a629557ff3ff0212577fe525
AdityA <aditya.ace93@gmail.com>
2023-08-31 10:07:03 +0530

    Update repeating_element.cpp

--- RoadMap/Array/repeating_element.cpp
+++ RoadMap/Array/repeating_element.cpp
@@ -14,7 +14,7 @@ using namespace std;
          
 int findRepeatingElement(int arr[], int n){      
     for (int i = 0; i < n; i++){    
-        for (int j = i+1; j < n; j++){
+        for (int j = i+1; j < n; j++){ 
             if (arr[i] == arr[j]){        
                 return arr[i];    
             }  
